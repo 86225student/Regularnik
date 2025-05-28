7ee5922dab5590d7bcb483badb76c225dd8ef254
Klaudia <klaudiakarczewskaa@gmail.com>
2025-05-28 14:09:44 +0200

    filtorowanie w widowku zawartości kursów; poprawa wyglądu: margines, padding, itp

--- ViewModels/CourseWordsViewModel.cs
+++ ViewModels/CourseWordsViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Input;
 using Regularnik.Models;
@@ -8,33 +11,37 @@ using Regularnik.Services;
 
 namespace Regularnik.ViewModels
 {
-    public class CourseWordsViewModel
+    public class CourseWordsViewModel : INotifyPropertyChanged
     {
-        // Nazwa kursu, np. "Kurs: Angielski"
         public string Header { get; }
-
-        // Kolekcja słówek wyświetlana w widoku
         public ObservableCollection<Word> Words { get; } = new ObservableCollection<Word>();
+        private readonly List<Word> _allWords = new List<Word>();
 
-        // Komendy
-        public ICommand EditCourseCommand { get; private set; }
-        public ICommand DeleteCourseCommand { get; private set; }
+        private string _searchTerm;
+        public string SearchTerm
+        {
+            get => _searchTerm;
+            set
+            {
+                if (_searchTerm == value) return;
+                _searchTerm = value;
+                OnPropertyChanged();
+                FilterWords();
+            }
+        }
 
-        // Czy edycja i usuwanie są dozwolone?
+        public ICommand EditCourseCommand { get; }
+        public ICommand DeleteCourseCommand { get; }
+
         public bool CanModify => !_protectedNames.Contains(_course.Name);
-
-        // Lista nazw chronionych przed edycją/usunięciem
         private static readonly string[] _protectedNames = { "A1", "A2", "B1", "B2" };
 
-        // Prywatne pola
         private readonly Course _course;
         private readonly Action<Course> _onEdit;
         private readonly Action _onDelete;
         private readonly DatabaseService _db;
 
-        /// <summary>
-        /// Główny konstruktor: przygotowuje listę słówek i komendy.
-        /// </summary>
+        // 1) Główny ctor z wszystkimi parametrami
         public CourseWordsViewModel(
             DatabaseService db,
             Course course,
@@ -47,48 +54,62 @@ namespace Regularnik.ViewModels
             _onDelete = onDelete ?? throw new ArgumentNullException(nameof(onDelete));
 
             Header = $"Kurs: {_course.Name}";
-
-            // Załaduj słówka z bazy
             foreach (var w in _db.GetWords(_course.Id))
+            {
                 Words.Add(w);
+                _allWords.Add(w);
+            }
 
-            // Edycja kursu
-            EditCourseCommand = new RelayCommand(
-                _ => _onEdit(_course),
-                _ => CanModify
-            );
+            EditCourseCommand = new RelayCommand(_ => _onEdit(_course), _ => CanModify);
+            DeleteCourseCommand = new RelayCommand(_ =>
+            {
+                var result = MessageBox.Show(
+                    $"Na pewno usunąć kurs „{_course.Name}” i wszystkie jego słowa?",
+                    "Potwierdź usunięcie",
+                    MessageBoxButton.YesNo,
+                    MessageBoxImage.Warning);
 
-            // Usuwanie kursu
-            DeleteCourseCommand = new RelayCommand(
-                _ =>
+                if (result == MessageBoxResult.Yes)
                 {
-                    var result = MessageBox.Show(
-                        $"Na pewno usunąć kurs „{_course.Name}” i wszystkie jego słowa?",
-                        "Potwierdź usunięcie",
-                        MessageBoxButton.YesNo,
-                        MessageBoxImage.Warning);
-
-                    if (result == MessageBoxResult.Yes)
-                    {
-                        // 1) Usuń wszystkie słowa tego kursu
-                        _db.DeleteWordsNotInCourse(_course.Id, new System.Collections.Generic.List<int>());
-                        // 2) Usuń sam kurs
-                        _db.DeleteCourse(_course.Id);
-                        // 3) Cofnij widok i odśwież listę
-                        _onDelete();
-                    }
-                },
-                _ => CanModify
-            );
+                    _db.DeleteWordsNotInCourse(_course.Id, new List<int>());
+                    _db.DeleteCourse(_course.Id);
+                    _onDelete();
+                }
+            }, _ => CanModify);
         }
 
-        /// <summary>
-        /// Przeciążenie dla zachowania kompatybilności:
-        /// jeśli nie podano onEdit/onDelete, przyciski po prostu nic nie robią.
-        /// </summary>
+        // 2) Convenience ctor bez onEdit/onDelete
         public CourseWordsViewModel(DatabaseService db, Course course)
             : this(db, course, _ => { }, () => { })
+        { }
+
+        // 3) **Nowy** ctor bez parametrów – to on usuwa błędy w XAML
+        public CourseWordsViewModel()
+            : this(new DatabaseService(), new Course { Id = 0, Name = "" })
+        { }
+
+        private void FilterWords()
         {
+            if (string.IsNullOrWhiteSpace(_searchTerm))
+            {
+                Words.Clear();
+                foreach (var w in _allWords) Words.Add(w);
+                return;
+            }
+
+            var term = _searchTerm.Trim();
+            var filtered = _allWords
+                .Where(w =>
+                    w.WordPl.IndexOf(term, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                    w.WordEn.IndexOf(term, StringComparison.OrdinalIgnoreCase) >= 0)
+                .ToList();
+
+            Words.Clear();
+            foreach (var w in filtered) Words.Add(w);
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
     }
 }
--- Views/CourseWordsView.xaml.cs
+++ Views/CourseWordsView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Regularnik.Views
 {
@@ -7,6 +9,32 @@ namespace Regularnik.Views
         public CourseWordsView()
         {
             InitializeComponent();
+        }
+
+        // Po naciśnięciu Enter: odfokusowanie TextBoxa
+        private void SearchBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                var scope = FocusManager.GetFocusScope((DependencyObject)sender);
+                FocusManager.SetFocusedElement(scope, null);
+                Keyboard.ClearFocus();
+            }
+        }
+
+        // Gdy TextBox traci fokus: odfokusowanie
+        private void SearchBox_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            var scope = FocusManager.GetFocusScope((DependencyObject)sender);
+            FocusManager.SetFocusedElement(scope, null);
+            Keyboard.ClearFocus();
+        }
+
+        // Kliknięcie w tło UserControl: przeniesienie fokusu stamtąd
+        private void UserControl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Możemy wyczyścić aktualny fokus
+            Keyboard.ClearFocus();
         }
     }
 }
